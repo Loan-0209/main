6300e055ad39ba6b2792f3b5e21b0d867437488e
Loan-0209 <Loan.vt@choiceind.com>
2025-05-28 16:08:53 +0700

    Add index.js file to complete basic project structure

---
+++ index.js
@@ -0,0 +1,12 @@
+// Simple Node.js application
+console.log('Hello from the main repository!');
+
+// Basic function to demonstrate testing
+function add(a, b) {
+  return a + b;
+}
+
+// Export for testing
+module.exports = {
+  add
+};
